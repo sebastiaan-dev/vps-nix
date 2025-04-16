0cb5c18b3b720ab03c15b3493fec15e2e52f642e
Sebastiaan Gerritsen <s.e.gerritsen@outlook.com>
2025-04-16 19:04:46 +0200

    Added home manager to dependencies

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
         };
     };
 
-    outputs = { self, nixpkgs, ... }@inputs: 
+    outputs = { self, nixpkgs, home-manager, ... }@inputs: 
     let 
         lib = nixpkgs.lib;
         common = [ ./modules/common/configuration.nix ];
