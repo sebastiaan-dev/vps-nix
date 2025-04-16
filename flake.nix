06e2f37b1ec0232ce0884ddf14c0a093895c7317
Sebastiaan Gerritsen <s.e.gerritsen@outlook.com>
2025-04-16 19:01:03 +0200

    Added home manager

--- flake.nix
+++ flake.nix
@@ -2,7 +2,15 @@
     description = "NixOS configuration";
 
     inputs = {
-       nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+        home-manager = {
+            url = "github:nix-community/home-manager/release-24.11";
+            # The `follows` keyword in inputs is used for inheritance.
+            # Here, `inputs.nixpkgs` of home-manager is kept consistent with
+            # the `inputs.nixpkgs` of the current flake,
+            # to avoid problems caused by different versions of nixpkgs.
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
     };
 
     outputs = { self, nixpkgs, ... }@inputs: 
@@ -23,6 +31,12 @@
             };
             net-worker-1 = lib.nixosSystem {
                 modules = common ++ [
+                    home-manager.nixosModules.home-manager
+                    {
+                        home-manager.useGlobalPkgs = true;
+                        home-manager.useUserPackages = true;
+                        home-manager.users.sebastiaan = import ./modules/home.nix;
+                    }
                     ./modules/oracle/net-worker-1/configuration.nix
                     ./modules/common/dns/dns.nix
                     ./modules/common/tailscale.nix
---
+++ modules/home.nix
@@ -0,0 +1,43 @@
+{ config, pkgs, ... }:
+
+{
+    home = {
+        username = "sebastiaan";
+        homeDirectory = "/home/sebastiaan";
+    };
+
+    programs.git = {
+        enable = true;
+        userName = "Sebastiaan Gerritsen";
+        userEmail = "dev@sebastiaan.io";
+    };
+
+    programs.starship = {
+        enable = true;
+    };
+
+    programs.zsh = {
+        enable = true;
+        enableCompletion = true;
+        enableBashCompletion = true;
+        autosuggestions.enable = true;
+        syntaxHighlighting.enable = true;
+
+        shellAliases = {
+            rebuild = "sudo nixos-rebuild switch --flake /home/sebastiaan/vps-nix/";
+        };
+    };
+
+    # This value determines the home Manager release that your
+    # configuration is compatible with. This helps avoid breakage
+    # when a new home Manager release introduces backwards
+    # incompatible changes.
+    #
+    # You can update home Manager without changing this value. See
+    # the home Manager release notes for a list of state version
+    # changes in each release.
+    home.stateVersion = "24.11";
+
+    # Let home Manager install and manage itself.
+    programs.home-manager.enable = true;
+}
