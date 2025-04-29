f8b6774b3aa1c8b48a2aa8b55035cfae4ca7a91c
Sebastiaan Gerritsen <s.e.gerritsen@outlook.com>
2025-04-29 17:25:35 +0200

    Remove disko tarball

--- modules/oracle/net-worker-2/configuration.nix
+++ modules/oracle/net-worker-2/configuration.nix
@@ -4,7 +4,6 @@
     imports =
     [
         ./hardware-configuration.nix
-        "${ builtins.fetchTarball { url = "https://github.com/nix-community/disko/archive/v1.11.0.tar.gz"; sha256 = "13brimg7z7k9y36n4jc1pssqyw94nd8qvgfjv53z66lv4xkhin92"; } }/module.nix"
         ./disk-config.nix
     ];
 
