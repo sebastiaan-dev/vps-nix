e1cb5adaf0be1773aa03be2c75e9a1557ac30ba2
Sebastiaan Gerritsen <s.e.gerritsen@outlook.com>
2025-05-10 14:43:55 +0200

    Changed EFI to BIOS for net-storage-1

--- modules/machines/host-c/net-storage-1/configuration.nix
+++ modules/machines/host-c/net-storage-1/configuration.nix
@@ -9,10 +9,7 @@
     ];
 
     boot.supportedFilesystems = [ "zfs" ];
-    boot.loader.grub = {
-      efiSupport = true;
-      efiInstallAsRemovable = true;
-    };
+    boot.loader.grub.device = "/dev/sdb";
 
     systemd.targets.multi-user.enable = true;
 
@@ -22,14 +19,42 @@
         wget
     ];
 
+    # 2: eth0: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 qdisc pfifo_fast state UP group default qlen 1000
+    # link/ether bc:24:11:88:8b:d1 brd ff:ff:ff:ff:ff:ff
+    # altname enp0s18
+    # inet 193.32.87.170/24 brd 193.32.87.255 scope global eth0
+    #    valid_lft forever preferred_lft forever
+    # inet6 2a0d:8143:0:b9::/64 scope global 
+    #    valid_lft forever preferred_lft forever
+    # inet6 fe80::be24:11ff:fe88:8bd1/64 scope link 
+    #    valid_lft forever preferred_lft forever
+
     # Disable autologin.
     services.getty.autologinUser = null;
     # Open ports in the firewall.
     networking = {
         hostId = "0654186d"; # From head -c 8 /etc/machine-id
         hostName = "net-storage-1";
-        # Dynamically configure networking
-        networkmanager.enable = true;
+        
+        interfaces = {
+          eth0 = {
+            ipv4.addresses = [{
+                address = "193.32.87.170";
+                prefixLength = 24;
+            }];
+            ipv6.addresses = [{
+                address = "2a0d:8143:0:b9::";
+                prefixLength = 64;
+            }];
+          };
+        };
+
+        defaultGateway = {
+            address = "193.32.87.1";
+            interface = "eth0";
+        };
+
+        nameservers = ["8.8.8.8" "1.1.1.1"];
 
         firewall = {
             # Always allow traffic from your Tailscale network
--- modules/machines/host-c/net-storage-1/disk-config.nix
+++ modules/machines/host-c/net-storage-1/disk-config.nix
@@ -2,8 +2,8 @@
 {
   disko.devices = {
     disk = {
-      disk1 = {
-        device = lib.mkDefault "/dev/sdb";
+      main = {
+        device = "/dev/sdb";
         type = "disk";
         content = {
           type = "gpt";
@@ -13,51 +13,39 @@
               size = "1M";
               type = "EF02";
             };
-            esp = {
-              name = "ESP";
-              size = "500M";
-              type = "EF00";
+            root = {
+              end = "-1G";
               content = {
                 type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
+                format = "ext4";
+                mountpoint = "/";
               };
             };
-            root = {
-              name = "root";
+            encryptedSwap = {
+              size = "10M";
+              content = {
+                type = "swap";
+                randomEncryption = true;
+                priority = 100; # prefer to encrypt as long as we have space for it
+              };
+            };
+            plainSwap = {
               size = "100%";
               content = {
-                type = "lvm_pv";
-                vg = "pool";
+                type = "swap";
+                discardPolicy = "both";
+                resumeDevice = true; # resume from hiberation from this device
               };
             };
           };
         };
       };
-      disk2 = {
-        device = lib.mkDefault "/dev/sda";
+      data = {
+        device = "/dev/sda";
         type = "disk";
         content = {
           type = "zfs";
           pool = "zroot";
-        };
-      };
-    };
-    lvm_vg = {
-      pool = {
-        type = "lvm_vg";
-        lvs = {
-          root = {
-            size = "100%FREE";
-            content = {
-              type = "filesystem";
-              format = "ext4";
-              mountpoint = "/";
-              mountOptions = [
-                "defaults"
-              ];
-            };
-          };
         };
       };
     };
